0b01091adce97100ef072a7e7fa7fc192e9ee05b
alexnascimento7788 <alexnascimento7788@gmail.com>
2026-04-09 19:26:12 -0300

    Ajustes realizados na segunda máquina

--- src/main/java/ConexaoDB.java
+++ src/main/java/ConexaoDB.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 
 public class ConexaoDB {
     // Restaurando o banco correto: retguarda
-    private static final String URL = "jdbc:mysql://10.0.0.116:3306/retguarda?useSSL=false";
+    private static final String URL = "jdbc:mysql://10.0.0.102:3306/retguarda?useSSL=false";
     private static final String USUARIO = "root";
     private static final String SENHA = "infor";
 
